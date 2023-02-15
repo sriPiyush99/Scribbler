972cce70af966ae80787c5e86d2c990e77baaab5
srivastavapiyush735@gmail.com <srivastavapiyush735@gmail.com>
2023-02-16 01:56:47 +0530

    This is Scribbler Project/Upgrad

---
+++ bloglist.js
@@ -0,0 +1,5 @@
+//  Use to navigate to next page
+
+function PostDetailPage() {
+  window.location.href = "http://127.0.0.1:5500/post.html";
+}
---
+++ index.js
@@ -0,0 +1,5 @@
+//  Use to navigate to All post page
+
+function AllPost() {
+  window.location.href = "http://127.0.0.1:5500/bloglist.html";
+}
